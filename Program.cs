c04d79a4fb4257744c350c3a7a7d74a4a27e5089
fogelt <fogeltt@gmail.com>
2025-10-02 10:28:55 +0200

    Merged master into branch

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ namespace Refactor
     {
         static List<Customers> customers = new List<Customers>
         {
-            new Customers { Id=1, Status=1, Amount=99.99m, Name="A" },
-            new Customers { Id=2, Status=0, Amount=49.50m, Name="B" },
-            new Customers { Id=3, Status=1, Amount=10m, Name="B" }
+            new Customers { Id=1, Approved=true, Amount=99.99m, Name="A" },
+            new Customers { Id=2, Approved=false, Amount=49.50m, Name="B" },
+            new Customers { Id=3, Approved=true, Amount=10m, Name="B" }
         };
 
         static void Main(string[] args)
@@ -31,59 +31,17 @@ namespace Refactor
                     case "0":
                         if (customers != null)
                         {
-<<<<<<< HEAD
-                            var d = customers.Select(z => z.Name).Distinct().ToList();
-                            for (int j = 0; j < d.Count; j++)
-                            {
-                                Console.WriteLine(d[j]);
-                            }
-                        }
-                    }
-                }
-                else if (x == "1")
-                {
-                    if (customers != null)
-                    {
-                        if (customers.Count > 0)
-                        {
-                            decimal s = 0;
-                            foreach (var i in customers)
-                            {
-                                if (i.Status == 1) // 1 = approved
-                                {
-                                    s = s + i.Amount;
-=======
+
                             if (customers.Count > 0)
                             {
-                                var d = customers.Select(z => z.cust).Distinct().ToList();
+                                var d = customers.Select(z => z.Name).Distinct().ToList();
                                 for (int j = 0; j < d.Count; j++)
                                 {
                                     Console.WriteLine(d[j]);
->>>>>>> master
                                 }
                             }
                         }
-<<<<<<< HEAD
-                    }
-                }
-                else if (x == "2")
-                {
-                    Console.WriteLine("Enter customer");
-                    var c = Console.ReadLine();
-                    var f = customers.Where(z => z.Name == c && z.Status == 1).ToList();
-                    for (int j = 0; j < f.Count; j++)
-                    {
-                        Console.WriteLine("ID: " + f[j].Id + " Amount: " + f[j].Amount);
-                    }
-                }
-                else if (x == "3")
-                {
-                    Console.WriteLine("bye");
-                }
-                else
-                {
-                    Console.WriteLine("err");
-=======
+
                         break;
 
                     case "1":
@@ -94,9 +52,9 @@ namespace Refactor
                                 decimal s = 0;
                                 foreach (var i in customers)
                                 {
-                                    if (i.sts == 1) // 1 = approved
+                                    if (i.Approved)
                                     {
-                                        s = s + i.amt;
+                                        s = s + i.Amount;
                                     }
                                 }
                                 Console.WriteLine("Sum of approved order: " + s);
@@ -107,10 +65,10 @@ namespace Refactor
                     case "2":
                         Console.WriteLine("Enter customer");
                         var c = Console.ReadLine();
-                        var f = customers.Where(z => z.cust == c && z.sts == 1).ToList();
+                        var f = customers.Where(z => z.Name == c && z.Approved).ToList();
                         for (int j = 0; j < f.Count; j++)
                         {
-                            Console.WriteLine("ID: " + f[j].id + " AMT: " + f[j].amt);
+                            Console.WriteLine("ID: " + f[j].Id + " AMT: " + f[j].Amount);
                         }
                         break;
 
@@ -121,7 +79,6 @@ namespace Refactor
                     default:
                         Console.WriteLine("err");
                         break;
->>>>>>> master
                 }
             }
 
